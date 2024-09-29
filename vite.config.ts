5e9c5b48798a74c3b5b435ad0576d8e73ca61269
azevedorar <lucas.timmy1@gmail.com>
2024-09-29 16:54:20 -0300

    Configure deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: '/lildigs-siteofc/', // Substitua 'nome-do-repo' pelo nome do seu repositório
+});
