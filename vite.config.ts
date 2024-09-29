c38aa4374b30461cfd36cc3b439611dd859c1506
azevedorar <lucas.timmy1@gmail.com>
2024-09-29 17:44:28 -0300

    version7

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/lildigs-siteofc/', // Substitua 'nome-do-repo' pelo nome do seu repositório
+  base: '/lildigs-siteofc/', // Substitua pelo nome correto do seu repositório
+  plugins: [react()],
 });
